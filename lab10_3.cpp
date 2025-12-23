7ac9aef98a4c94fcc8c2152bf1045b335719a494
nemonapatsadon-source <nemonapatsadon@gmail.com>
2025-12-23 16:36:55 +0700

    lab10-3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,55 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+
+#include <iomanip>
+
+#include <cmath>
+
+#include <string>
+
+#include<fstream>
+
+
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+        int count = 0;
+
+        float sum = 0;
+
+        float sum_of_square = 0;
+
+        string textline;
+
+        float Avg ;
+
+        float SD ;
+
+        ifstream source("score.txt");
+
+        while (getline(source,textline))
+
+        {
+
+                sum += atof(textline.c_str());
+                sum_of_square += pow(atof(textline.c_str()),2);
+                count++;
+
+        }
+
+
+
+        cout << "Number of data = " << count << "\n";
+
+        cout << setprecision(3);
+
+        cout << "Mean = " << sum/count << "\n";
+
+        cout << "Standard deviation = " << pow(((sum_of_square/count)-pow( sum/count,2)),0.5) ;
+
+}
